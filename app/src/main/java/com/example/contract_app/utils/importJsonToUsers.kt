8ff07a66d2788f567f569e9556c4e2544fadbc06
ducphu <buiphu05012002@gmail.com>
2024-10-24 18:05:11 +0700

    sdsd

--- app/src/main/java/com/example/contract_app/utils/importJsonToUsers.kt
+++ app/src/main/java/com/example/contract_app/utils/importJsonToUsers.kt
@@ -9,6 +9,7 @@ fun importJsonToUsers(jsonString: String): List<User> {
     val gson = Gson()
     val userType = object : TypeToken<List<User>>() {}.type
     val usersFromJson: List<User> = gson.fromJson(jsonString, userType)
+
     return usersFromJson.map {
         it.copy(id = UUID.randomUUID().toString())
     }
